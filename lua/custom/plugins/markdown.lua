ebacfe97cd474ee117b64995e66986fc16fb3f93
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-09-09 16:03:47 +0700

    🛠️ Lazy load markdown plugin

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,6 +1,7 @@
 return {
   "iamcco/markdown-preview.nvim",
-  event = "BufEnter *.md",
+  -- event = "BufEnter *.md",
+  cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
   config = function()
     vim.fn["mkdp#util#install"]()
   end,
