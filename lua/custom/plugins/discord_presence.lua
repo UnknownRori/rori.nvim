06f1fba88b09dbed23b09130c668e065b08846d5
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-03-06 05:44:04 +0700

    📦 added workaround so workspace can be displayed

--- lua/custom/plugins/discord_presence.lua
+++ lua/custom/plugins/discord_presence.lua
@@ -1,13 +1,30 @@
+local function workspace(name)
+  if name == "" or name == nil then
+    local pwd = vim.fn.getcwd()
+    local name = vim.fn.fnamemodify(pwd, ':t');
+
+    if name == "nvim" then
+      return "Wasting time on my config"
+    end
+
+    return "🔨 Working on " .. name
+  end
+
+  return "Woopsi, have a nice day :)"
+end
+
 return {
   'andweeb/presence.nvim',
   event = "BufReadPost",
   opts = {
+    buttons             = true,
+
     editing_text        = "🛠️ Editing %s",
     file_explorer_text  = "📝 Browsing %s",
     git_commit_text     = "📝 Committing changes",
     plugin_manager_text = "📦 Managing plugins",
     reading_text        = "📝 Reading %s",
-    workspace_text      = "🔨 Working on %s",
+    workspace_text      = workspace,
     line_number_text    = "📝 Line %s out of %s",
   }
 }
